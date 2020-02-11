cc068401525bac218101b46915a006c11f282bc7
Giuseppe Fierro <fierro.gpp@gmail.com>
2020-02-11 19:56:26 +0100

    Theme, Navbar, Loading

--- app/index.js
+++ app/index.js
@@ -1,8 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Loading from './components/Loading'
+import Nav from './components/Nav'
+import { ThemeProvider } from './contexts/theme'
+import './index.css'
 
 function App() {
-  return <div>Jello world</div>
+  const [theme, setTheme] = React.useState('light')
+  const toggleTheme = () => setTheme(theme => theme === 'light' ? 'dark' : 'light')
+
+  return (
+    <Router>
+      <ThemeProvider value={theme}>
+        <div className={theme}>
+          <div className="container">
+            <Nav toggleTheme={toggleTheme}/>
+            <React.Suspense fallback={<Loading />}>
+              <Switch>
+                <Route exact path="/" render={() => <h1>top</h1>}/>
+                <Route exact path="/new" render={() => <h1>new</h1>}/>
+                <Route render={() => <h1>404</h1>}/>
+              </Switch>
+            </React.Suspense>
+          </div>
+        </div>
+      </ThemeProvider>
+    </Router>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
