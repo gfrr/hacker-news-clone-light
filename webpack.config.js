5afcb0f8abb7580c6393d4caa39774537d57990a
Giuseppe Fierro <fierro.gpp@gmail.com>
2021-04-08 15:39:47 +0200

    npm audit fix

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     }),
-    new CopyPlugin([
+    new CopyPlugin({
+      patterns: [
       { from: '_redirects' }
-    ])
+    ]})
   ],
   devServer: {
     historyApiFallback: true
